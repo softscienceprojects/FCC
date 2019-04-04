1bc4593ae4e43441f2f24e7507c604211a15faf8
erin <erin.nicole.johnson@gmail.com>
2019-04-04 16:52:57 +0100

    added my comments

--- SliceAndSplice.js
+++ SliceAndSplice.js
@@ -9,13 +9,13 @@ Return the resulting array. The input arrays should remain the same after the fu
 
 
 
-function frankenSplice(arr1, arr2, n) {
+function frankenSplice(arr1, arr2, n) {   // function parameters
   // It's alive. It's alive!
-  var copyArr = arr2.slice();
+  var copyArr = arr2.slice();  // slice array 2 into your empty array (slice makes a copy)
   
-  for (let i = 0; i<arr1.length; i++) {
-    copyArr.splice(n, 0, arr1[i]);
-    n++;
+  for (let i = 0; i<arr1.length; i++) { // for loop the entire length of the 1st array
+    copyArr.splice(n, 0, arr1[i]);  // splice (mutable) at nth index, take out 0 items, add in each arr1 item
+    n++; //increment
   }
   console.log(copyArr);
   return copyArr;
