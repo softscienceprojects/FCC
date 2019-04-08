670a7a0852ce5171c9cfecbbee4a4a493661ca24
erin <erin.nicole.johnson@gmail.com>
2019-04-08 21:01:10 +0100

    Update Mutations.js
    A slightly inelegant solution.

--- Mutations.js
+++ Mutations.js
@@ -6,15 +6,17 @@ second element of the array.
 
 
 function mutation(arr) {
-  var origin = arr[0];
-  var emptyArr = "["+arr[1] +"+]";
-  var checking = RegExp(emptyArr, "gi"); 
-  console.log(checking);
-  var found = checking.test(origin);
-  console.log(found);
-  var hardTest = /Aarmy/gi;
-  console.log(hardTest.test(origin));
-  return found;
+  var origin = arr[0].toLowerCase().split('');
+  var lookup = arr[1].toLowerCase().split('');
+  var check = true;
+
+  lookup.forEach(function(element) {
+    if (origin.indexOf(element) === -1) {
+      console.log(origin.indexOf(element));
+      check = false;
+    } 
+  });
+ return check;
 }
 
-mutation(["Mary", "Aarmy"]); 
+mutation(["hello", "hey"]);
