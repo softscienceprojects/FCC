9c969a9b6012197ce9b5531557092ef79aceb77c
erin <erin.nicole.johnson@gmail.com>
2019-04-13 14:46:43 +0100

    added comments
    with help from scotch.io

--- ChunkyMonkey.js
+++ ChunkyMonkey.js
@@ -5,14 +5,19 @@ them as a two-dimensional array. */
 function chunkArrayInGroups(arr, size) {
   // Break it up.
 
-   var myArray= [];
+   var myArray= []; // new array
 
-  
-  for (let i=0; i<arr.length; i += size) {
+  //iterate the length of the original array, by the size of the desired array
+  // ('to start each new group where the previous one ended')
+  for (let i=0; i<arr.length; i += size) { 
+  // create a new 'chunk' variable, which is the original array sliced from i (0)
+    //to the end of the chunk's size
     let group = arr.slice(i, i+size);
+    //push to your new array
     myArray.push(group);
   }
   console.log(myArray);
+  //return the result
   return myArray;
 }
 
