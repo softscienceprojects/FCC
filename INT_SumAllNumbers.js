8d04eebcc5d470f6de3c57920a3b18c42850eff9
erin <erin.nicole.johnson@gmail.com>
2019-04-13 16:30:24 +0100

    Added updated comments

--- INT_SumAllNumbers.js
+++ INT_SumAllNumbers.js
@@ -7,15 +7,15 @@ The lowest number will not always come first. */
 function sumAll(arr) {
   
   arr.sort((a,b) => a-b); // always sort smallest to biggest
-  let a = arr[0], b = arr[1];
-  var myArray = []
+  let a = arr[0], b = arr[1]; // declare them as variables, with the first item and the last item
+  var myArray = [] //create an empty array
   
 
-  while (a<=b) {
-    myArray.push(a++)
+  while (a<=b) { // while a is less than or equal to b
+    myArray.push(a++) //push each number onto your array
   }
 
-  let total = myArray.reduce((sum, item) => sum + item);
+  let total = myArray.reduce((sum, item) => sum + item); // use reduce to sum each item in the array
   console.log(total);
   return total;
 }
