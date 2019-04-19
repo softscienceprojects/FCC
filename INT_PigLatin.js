cd2ec9d9370dfa0e7433f0c1fa8b06835172a042
erin <erin.nicole.johnson@gmail.com>
2019-04-19 17:26:35 +0100

    Update INT_PigLatin.js
    added else if in the middle

--- INT_PigLatin.js
+++ INT_PigLatin.js
@@ -6,19 +6,21 @@ function translatePigLatin(str) {
   // Does it start with a vowel? Yes, add '-way'
   if (str[0]==allVowels) {
     ourSolution = str + "way";
+  } 
+  else if (str.match(allMyVowels) === null) {
+    ourSolution = str + "ay";
   }
   // Does not have a vowel? look through the word, find the first vowel! 
     else {
       for (let i =0; i<=str.length;i++) {
-        console.log(str[i]);
         if (str[i] == allVowels) {
           var first = str.indexOf(str[i]);
+          ourSolution =  str.slice(first)+ str.slice(0,first) + "ay";
         } 
-      } ourSolution =  str.slice(first)+ str.slice(0, first) + "ay";
+      } 
     }
-  
   console.log(ourSolution);
   return ourSolution; 
 }
 
-translatePigLatin("bzzz");
+translatePigLatin("bzpt");
