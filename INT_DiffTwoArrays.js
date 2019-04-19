d0adbd8cd02511ea1df63a0f6918db4895a625dc
erin <erin.nicole.johnson@gmail.com>
2019-04-19 15:47:35 +0100

    Update INT_DiffTwoArrays.js

--- INT_DiffTwoArrays.js
+++ INT_DiffTwoArrays.js
@@ -4,12 +4,33 @@ return the symmetric difference of the two arrays.
 */
 
 function diffArray(arr1, arr2) {
-  var newArr = [];
-  // Same, same; but different.
+ //var newArr = [];
+var newArr = arr1.concat(arr2);
+var blank = [];
 
+// var uniq = newArr.filter(function(item, index){
+// 	return newArr.indexOf(item) < index;
+// });
 
-  
+let uniq = newArr.filter(elm => {
+  return arr2.indexOf(elm) > -1 && arr1.indexOf(elm) > -1 ? true : false;
+})
+// let setArr = new Set([...newArr]);
+
+  // function inFirst (fir, sec) { 
+  //   for (let i=0;i<fir.length; i++) {
+  //     if (sec.indexOf(fir[i]) === -1) {
+  //         console.log(fir[i]);
+  //       newArr.push(fir[i]);
+  //     }
+  //   }
+  // }
+  // inFirst(arr1, arr2);
+  // inFirst(arr2, arr1);
+
+  console.log(uniq);
+  console.log(newArr);
   return newArr;
 }
 
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]);
