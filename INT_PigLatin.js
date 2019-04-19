b9e6379acc424ed56cfac81ae69839c9904a6a2b
erin <erin.nicole.johnson@gmail.com>
2019-04-19 15:34:09 +0100

    Update INT_PigLatin.js
    Trying this approach. How to get the loop to carry on & then push to array?

--- INT_PigLatin.js
+++ INT_PigLatin.js
@@ -1,31 +1,27 @@
 function translatePigLatin(str) {
-  var ourSolution = '';
+  var ourSolution = [];
   var pig = str.split(''); //split
-  let myFirstVowels = /^[aeiou]/i;
-    let firstVowel = str.match(myFirstVowels);
   let allMyVowels = /[aeiou]/i;
     let allVowels = str.match(allMyVowels);
 
   // Does it start with a vowel? Yes, add '-way'
-  if (str.charAt(0)==firstVowel) {
-    ourSolution = str + "way";
+  if (pig[0]==allVowels) {
+    ourSolution.push(str, "way");
   }
   // Does not have a vowel? look through the word, find the first vowel! 
     else {
-        // at what index are the values found? 
-      for (let i =0; i<str.length;i++) {
-        if (str.charAt(i) == allVowels) {
-        // take off the letters before the vowel
-          var consStart = str.slice(i-str.charAt(i), i);
-          var consEnding = str.slice(i);
-        } // put them at the end
-      } ourSolution = consEnding + consStart+"ay";
-    // add -ay
+      for (let i =0; i<=pig.length;i++) {
+        if (pig[i] == allVowels) {
+          ourSolution.push(pig.slice(pig[i-1], i));
+          break;
+        } console.log(ourSolution);
+          console.log(pig[i])
+      } 
     }
   
-  
+  ourSolution = ourSolution.join('');
   console.log(ourSolution);
-  return ourSolution;
+  return ourSolution; 
 }
 
-translatePigLatin("glove");
+translatePigLatin("california");
