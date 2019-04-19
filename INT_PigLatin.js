c94760bbbb3dfb107b2130f24756236323d42463
erin <erin.nicole.johnson@gmail.com>
2019-04-19 12:09:04 +0100

    Update INT_PigLatin.js
    A little further - found the index of the first vowel

--- INT_PigLatin.js
+++ INT_PigLatin.js
@@ -1,20 +1,10 @@
-/* Translate the provided string to pig latin.
-
-Pig Latin takes the first consonant (or consonant cluster) of an English word, 
-moves it to the end of the word and suffixes an "ay".
-
-If a word begins with a vowel you just add "way" to the end.
-
-Input strings are guaranteed to be English words in all lowercase. */
-
-
 function translatePigLatin(str) {
   var ourSolution = '';
   var pig = str.split(''); //split
   let myFirstVowels = /^[aeiou]/i;
-  let allVowels = /[aeiou]/i;
-  let firstVowel = str.match(myFirstVowels);
-console.log(firstVowel);
+    let firstVowel = str.match(myFirstVowels);
+  let allMyVowels = /[aeiou]/i;
+    let allVowels = str.match(allMyVowels);
 
   // Does it start with a vowel? Yes, add '-way'
   if (str.charAt(0)==firstVowel) {
@@ -25,27 +15,18 @@ console.log(firstVowel);
     else {
   // at what index are the values found? 
 for (let i =0; i<str.length;i++) {
-  if (str.charAt(i) == firstVowel) {
-
+  if (str.charAt(i) == allVowels) {
+   // take off the letters before the vowel
+    console.log(i);
   }
 };
-  
-  // take off the letters before the vowel
-
-
-
+ 
     // put them at the end
-
     // add -ay
-
-
     }
-
-
-  
   
   console.log(ourSolution);
   return ourSolution;
 }
 
-translatePigLatin("paragraphs");
+translatePigLatin("glove");
