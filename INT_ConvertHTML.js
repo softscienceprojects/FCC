69c67f8f743376ad3f0ef19100a294e3e5f8e4c2
erin <erin.nicole.johnson@gmail.com>
2019-04-28 17:07:47 +0100

    Update INT_ConvertHTML.js
    added in the advanced solution as well

--- INT_ConvertHTML.js
+++ INT_ConvertHTML.js
@@ -8,3 +8,25 @@ return str;
 }
 
 convertHTML("<>");
+
+/*  The advanced solution:
+function convertHTML(str) {
+      // Use Object Lookup to declare as many HTML entities as needed.
+      htmlEntities={
+        '&':'&amp;',
+        '<':'&lt;',
+        '>':'&gt;',
+        '"':'&quot;',
+        '\'':"&apos;"
+      };
+      //Use map function to return a filtered str with all entities changed automatically.
+      return str.split('').map(entity => htmlEntities[entity] || entity).join('');
+    }
+
+    // test here
+    convertHTML("Dolce & Gabbana");
+    
+    
+    Saving as I hadn't thought of using objects before (dictionaries!) as lookups for replacement. Storing this to 
+    remember the next time I come across a similar problem. 
+    */
