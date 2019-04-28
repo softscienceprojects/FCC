62c7da73ce70a5e2be90f6d4487f6f162b978d67
erin <erin.nicole.johnson@gmail.com>
2019-04-28 17:05:47 +0100

    Update INT_ConvertHTML.js
    The intermediate suggestion in the hint - far easier to work with and where I was trying to end up. No if statements needed, just evaluates each time it finds it. Good practice for RegEx

--- INT_ConvertHTML.js
+++ INT_ConvertHTML.js
@@ -2,34 +2,9 @@
 
 
 function convertHTML(str) {
-  var findChar = /[&<>"'{1,}]/;
-  console.log(findChar);
-  var replace = str.match(findChar);
-  console.log(replace)
-  var re = new RegExp(replace,"g");
-  var newStr = str;
-
-  // find out which one it is, then replace it
- 
-  if (replace == "&" ) {
-    newStr = str.replace(re, "&amp;");
-    }
-  if (replace == "<" ) {
-    newStr =  str.replace(re, "&lt;");
-    }
-  if (replace == ">" ) {
-    newStr =  str.replace(re, "&gt;");
-    }
-  if (replace == "\"") {
-    newStr = str.replace(re, "&quot;")
-  }  
-  if (replace == "'") {
-    newStr = str.replace(re, "&apos;")
-  }
-  return newStr;
-
+   str = str.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;').replace(/'/g,"&apos;");
+return str;
     
-
 }
 
 convertHTML("<>");
