2d200497e416a096b69b8688793621e3c7e321a3
erin <erin.nicole.johnson@gmail.com>
2019-05-21 19:14:46 +0100

    Update INT_SpinalCase.js
    Solved for whitespaces, not all words

--- INT_SpinalCase.js
+++ INT_SpinalCase.js
@@ -2,9 +2,13 @@ function spinalCase(str) {
   // "It's such a fine line between stupid, and clever."
   // --David St. Hubbins
 
-
+  var newStr = str.toLowerCase();
   
-  return str;
+  let codingRegex = /\s/g
+  let result = newStr.replace(codingRegex, "-");
+  console.log(result);
+
+  return result;
 }
 
 spinalCase('This Is Spinal Tap');
