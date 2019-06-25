ceffacb091d6774c29caa277a55fbc9989be5f23
erin <erin.nicole.johnson@gmail.com>
2019-06-25 21:43:49 +0100

    Create INT_Steamroller.js

---
+++ INT_Steamroller.js
@@ -0,0 +1,22 @@
+/*
+Flatten a nested array. You must account for varying levels of nesting.
+*/
+
+function steamrollArray(arr) {
+  // I'm a steamroller, baby
+
+//  var newArr = [] 
+//  for (let i = 0; i<arr.length; i++) {
+//    newArr.concat(arr[i])
+//  }
+//  console.log(newArr)
+
+  // var newArray = Array.prototype.concat.apply([], arr);
+  // console.log(newArray);
+  //  return newArray
+
+  var newArray = arr.flat(arr.length);
+  return newArray
+}
+
+steamrollArray([1, [], [3, [[4]]]]);
