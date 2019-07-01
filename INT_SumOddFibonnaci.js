6ce36d0b102c794ba6a9517e874a1365432e4d85
erin <erin.nicole.johnson@gmail.com>
2019-07-01 19:05:39 +0100

    Create INT_SumOddFibonnaci.js

---
+++ INT_SumOddFibonnaci.js
@@ -0,0 +1,27 @@
+/* Given a positive integer num, return the sum of all odd 
+Fibonacci numbers that are less than or equal to num.
+
+The first two numbers in the Fibonacci sequence are 1 and 1. 
+Every additional number in the sequence is the sum of the two 
+previous numbers. The first six numbers of the Fibonacci 
+sequence are 1, 1, 2, 3, 5 and 8.
+
+For example, sumFibs(10) should return 10 because all odd 
+Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5. */
+
+function sumFibs(num) {
+  var holdarr = []
+ 
+  console.log(num%2)
+  //return num;
+ 
+}
+ 
+sumFibs(9);
+ 
+
+// Fibonacci alg: xn = xn-1 + xn-2
+// keep going until you get to 0
+// odd: num%2 == 1
+// even: num%2 == 0
+
