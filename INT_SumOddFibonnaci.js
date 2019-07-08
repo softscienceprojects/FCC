d934bd144246306a1d2cfed8198f49171e1744bb
erin <erin.nicole.johnson@gmail.com>
2019-07-08 20:55:23 +0100

    getting closer
    got all tests except one to pass

--- INT_SumOddFibonnaci.js
+++ INT_SumOddFibonnaci.js
@@ -12,19 +12,35 @@ Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5. */
 function sumFibs(num) {
   var myArr = [1,1];
 
-  for (let i=1;i<=num; i++) {
+  for (let i=1;i<num; i++) {
     var counter = myArr[i] + (myArr[i-1])
-    myArr.push(counter);
+    if (counter < num) {
+      myArr.push(counter);
+    } 
   }
-  console.log(myArr)
-  return num;
-}
- 
-sumFibs(9);
- 
+  
+  var answer = 0;
 
-// Fibonacci alg: xn = xn-1 + xn-2
-// keep going until you get to 0
+  for (let i=0;i<myArr.length; i++) {
+    if (myArr[i]%2==1) {
+      answer += myArr[i]
+    }
+  }
+
+  console.log(answer)
+  return answer
+}
+
+sumFibs(4);
+
 // odd: num%2 == 1
 // even: num%2 == 0
 
+//Fibonacci alg: xn = xn-1 + xn-2
+
+  // var myArr = [1,1];
+
+  // for (let i=1;i<=num; i++) {
+  //   var counter = myArr[i] + (myArr[i-1])
+  //   myArr.push(counter);
+  // }
